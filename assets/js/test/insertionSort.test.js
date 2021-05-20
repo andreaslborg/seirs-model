caa9142bdb8795ac9fdc53db79eb9d4b2793412b
andreasborgaau <72602768+andreasborgaau@users.noreply.github.com>
2021-05-20 18:39:01 +0200

    Delete assets directory

--- assets/js/test/insertionSort.test.js
+++
@@ -1,8 +0,0 @@
-const insertionSort = require('./insertionSort');
-
-/* First element is undefined or equal to 0 */
-const arr = [undefined, 63792579848, 63792579868, 63792579850, 63792301041];
-
-test('insertionSort', () => {
-    expect(insertionSort(arr)).toEqual([0, 4, 1, 3, 2]);
-});
